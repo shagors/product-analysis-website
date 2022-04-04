055407d0a6176ab4e45b7ee0f614979398956b0a
shagors <shajjadshagor@gmil.com>
2022-04-04 10:54:38 +0600

    react 18 issue fixed and react router install

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOMClient from 'react-dom/client'
 
-ReactDOM.render(
+const root = ReactDOMClient.createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
